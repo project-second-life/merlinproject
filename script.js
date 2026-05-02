98c6efc50f3da77a6573c9b483637069714d5a5a
project-second-life <85352544+project-second-life@users.noreply.github.com>
2026-05-02 12:05:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const images = [
-        "assets/image1.jpg",
-        "assets/image2.jpg",
+        "assets/Image1.jpg",
+        "assets/Image2.jpg",
         "assets/image3.jpg",
         "assets/image4.jpg"
     ];
